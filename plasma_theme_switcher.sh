2b7e54dc6f5ecfc82a28bf39e93a30cdcbd65c20
Eduardo Flores <edfloreshz@gmail.com>
2020-10-08 22:53:25 -0700

    plasmarc path was incorrect.

--- plasma_theme_switcher.sh
+++ plasma_theme_switcher.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
 currenttime=$(date +%H:%M)
 while true
 do
-	if [[ "$currenttime" > "18:00" ]] || [[ "$currenttime" < "07:00" ]]; then
-		if grep -q 'default' plasmarc; then
-			lookandfeeltool -a org.kde.breezedark.desktop
-		fi
-	else
-		if grep -q 'breeze-dark' plasmarc; then
-			lookandfeeltool -a org.kde.breeze.desktop
-		fi
-	fi
+        if [[ "$currenttime" > "18:00" ]] || [[ "$currenttime" < "07:00" ]]; then
+                if grep -q 'default' ~/.config/plasmarc; then
+                        lookandfeeltool -a org.kde.breezedark.desktop
+                fi
+        else
+                if grep -q 'breeze-dark' ~/.config/plasmarc; then
+                        lookandfeeltool -a org.kde.breeze.desktop
+                fi
+        fi
 done
