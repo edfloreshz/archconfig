ef4a01f6c4978ea1765daab4b0988812fa21fd26
Eduardo Flores <edfloreshz@gmail.com>
2020-10-25 11:12:44 -0700

    Added script to install my programs.

--- plasma_theme_switcher.sh
+++ plasma_theme_switcher.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 currenttime=$(date +%H:%M)
 while true
 do
-        if [[ "$currenttime" > "18:00" ]] || [[ "$currenttime" < "07:00" ]]; then
-                if grep -q 'default' ~/.config/plasmarc; then
-                        lookandfeeltool -a org.kde.breezedark.desktop
-                fi
-        else
-                if grep -q 'breeze-dark' ~/.config/plasmarc; then
-                        lookandfeeltool -a org.kde.breeze.desktop
-                fi
+    if [[ "$currenttime" > "18:00" ]] || [[ "$currenttime" < "07:00" ]]; then
+        if grep -q 'default' ~/.config/plasmarc; then
+            lookandfeeltool -a org.kde.breezedark.desktop
         fi
+    else
+        if grep -q 'breeze-dark' ~/.config/plasmarc; then
+            lookandfeeltool -a org.kde.breeze.desktop
+        fi
+    fi
 done
