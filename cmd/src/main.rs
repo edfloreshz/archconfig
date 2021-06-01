6993ea388a0b61ba2e242247bd78e7487f152299
Eduardo Flores <edfloreshz@gmail.com>
2021-06-01 14:57:31 -0700

    Definicion de la CLI.

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -12,7 +12,45 @@ fn parse_args() -> ArgMatches<'static> {
     App::new("Dotsy")
         .version("0.1.0")
         .author("Eduardo F. <edfloreshz@gmail.com>")
-        .about("Confiuration manager for Dotsy.")
+        .about("Dotsy is a configuration manager for UNIX-based systems.")
+        .arg(
+            Arg::with_name("init")
+                .help("Creates local git repo to store dotfiles.")
+                .short("i")
+                .long("init"),
+        )
+        .arg(
+            Arg::with_name("pub")
+                .help("Publishes local git repository to desired provider.")
+                .short("pub")
+                .long("publish"),
+        )
+        .arg(
+            Arg::with_name("add")
+                .help("Adds a dotfile to track.")
+                .short("a")
+                .long("add")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("rem")
+                .help("Removes a dotfile from tracking.")
+                .short("r")
+                .long("remove")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("push")
+                .help("Push changes to git repo.")
+                .long("push")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("pull")
+                .help("Pull changes from git repo.")
+                .long("pull")
+                .takes_value(true),
+        )
         .arg(
             Arg::with_name("daemon")
                 .help("Starts Dotsy daemon and shows the output.")
