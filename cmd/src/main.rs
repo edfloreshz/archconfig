a940294f5804835982fef3eb4d94093b011eeb92
Eduardo Flores <edfloreshz@gmail.com>
2021-07-20 20:04:37 -0700

    Función main simplificada.

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -2,9 +2,7 @@ use dotsy_core::cmd::args::parse_args;
 use dotsy_core::models::command::Command;
 
 fn main() -> Result<(), std::io::Error> {
-    let matches = parse_args();
-    let checked = Command::new(&matches);
-    match checked {
+    match Command::new(&parse_args()) {
         Some(cmd) => cmd.execute()?,
         None => eprintln!("Type `dotsy help` if you need help."),
     }
